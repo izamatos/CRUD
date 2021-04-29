ef5c07c33c43895db5f314a631af4cab05283f88
izamatos <82831186+izamatos@users.noreply.github.com>
2021-04-29 12:25:58 -0300

    first commit
    • CRUD de Cliente
    • A aplicação permitirá ao usuário cadastrar clientes pelo Nome, CPF, Numero da conta , saldo e informar se é um cliente ativo ou não.
    • A aplicação permitirá ao usuário pesquisar clientes pelo CPF ou numero da conta
    • A aplicação permitirá que o usuário consiga excluir um cliente pelo seu numero de CPF.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Business;
+
+namespace CRUD
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+
+            while (!escolheuSair)
+            {
+                Console.Clear();
+                Console.WriteLine("==============================");
+                Console.WriteLine("      CRUD DE CLIENTES");
+                Console.WriteLine("==============================");
+                Console.WriteLine("1 - Cadastrar Cliente\n2 - Pesquisar Cliente\n3 - Lista de Clientes\n4 - Remover Cliente\n5 - Sair");
+                Console.WriteLine("Selecione a opçao desejada: ");
+                string opçao = Console.ReadLine();
+                int opint = int.Parse(opçao);
+                if (opint == 5) break;
+
+                CrudClienteServices.opçaoSelecionada(opint);
+            }
+            Console.Clear();
+        }
+
+    }
+}
+
